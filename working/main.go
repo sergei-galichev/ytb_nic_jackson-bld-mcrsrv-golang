bc5dd1373f8b68d579ee986cafaafe0f4edd0b06
Sergei Galichev <sergey.galichev@outlook.com>
2023-11-14 13:27:22 +1000

    add a one more handler "/bye"

--- working/main.go
+++ working/main.go
@@ -9,5 +9,8 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("Hello, World!")
 	})
+	http.HandleFunc("/bye", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Bye, World!")
+	})
 	http.ListenAndServe(":9090", nil)
 }
