cb967f8f14c4c02d3455a01c4b419314b71c339f
Sergei Galichev <sergey.galichev@outlook.com>
2023-11-14 13:45:48 +1000

    changes handler "/": read data from Body and print formatted string

--- working/main.go
+++ working/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -8,6 +10,11 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("Hello, World!")
+		d, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Fprintf(w, "Hello, %s", d)
 	})
 	http.HandleFunc("/bye", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("Bye, World!")
