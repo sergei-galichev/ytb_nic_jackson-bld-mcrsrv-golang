44535113f10b45f1fc51ff3207a2a9f07c809e7a
Sergei Galichev <sergey.galichev@outlook.com>
2023-11-14 13:05:53 +1000

    create a simple http server

---
+++ working/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Hello, World!")
+	})
+	http.ListenAndServe(":9090", nil)
+}
