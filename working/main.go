ea43e4f248e094356b316a0eaaded7471f27d942
Sergei Galichev <sergey.galichev@outlook.com>
2023-11-15 12:56:32 +1000

    create: package handlers; hello.go with code of our Hello World function in main

--- working/main.go
+++ working/main.go
@@ -1,24 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Hello, World!")
-		d, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, "Oops, something went wrong", http.StatusBadRequest)
-			return
-		}
-		fmt.Fprintf(w, "Hello, %s\n", d)
-	})
-	http.HandleFunc("/bye", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Bye, World!")
-	})
+
 	http.ListenAndServe(":9090", nil)
 }
