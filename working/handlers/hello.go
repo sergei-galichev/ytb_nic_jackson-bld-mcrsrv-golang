59ee47531468dd7e7a1cd2fa2872b465ce7d4f3a
Sergei Galichev <sergey.galichev@outlook.com>
2023-11-15 16:59:20 +1000

    some fixes in logging

--- working/handlers/hello.go
+++ working/handlers/hello.go
@@ -16,10 +16,11 @@ func NewHello(l *log.Logger) *Hello {
 }
 
 func (h *Hello) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	h.l.Println("Hello World!")
+	h.l.Println("Handling Hello Request")
 	d, err := io.ReadAll(r.Body)
 	if err != nil {
-		http.Error(w, "Oops, something went wrong", http.StatusBadRequest)
+		h.l.Println("Error reading request body", err)
+		http.Error(w, "Unable to read request body", http.StatusBadRequest)
 		return
 	}
 	fmt.Fprintf(w, "Hello, %s\n", d)
