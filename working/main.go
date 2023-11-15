41bdc476ce25fd72e14e4f124c4235cbb6e344a1
Sergei Galichev <sergey.galichev@outlook.com>
2023-11-15 17:01:38 +1000

    changes: create HTTP server with parameters; added code for starting server in gorutine and notify if process is interrupted

--- working/main.go
+++ working/main.go
@@ -1,18 +1,50 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 	"ytb_nic_jackson-bld-mcrsrv-golang/working/handlers"
 )
 
 func main() {
 	l := log.New(os.Stdout, "product-api: ", log.LstdFlags)
+
 	hh := handlers.NewHello(l)
 	gh := handlers.NewGoodbye(l)
+
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
 	sm.Handle("/goodbye", gh)
-	http.ListenAndServe(":9090", sm)
+
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		ErrorLog:     l,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+
+	go func() {
+		l.Println("Starting server on port 9090")
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Printf("Error starting server: %s\n", err)
+			os.Exit(1)
+		}
+	}()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 }
