b9b0473ebecfbb4744242c311002728ad90989d3
Sergei Galichev <sergey.galichev@outlook.com>
2023-11-17 16:19:18 +1000

    added POST method addProduct() added comments added code to check what type of method in request

--- product-api/handlers/products_handler.go
+++ product-api/handlers/products_handler.go
@@ -14,10 +14,36 @@ func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
+// ServeHTTP is the main entry point for the handler and staisfies the http.Handler interface
 func (p *Products) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		p.getProducts(w, r)
+		return
+	} else if r.Method == http.MethodPost {
+		p.addProduct(w, r)
+		return
+	}
+
+	// catch all
+	//if no method is matched
+	w.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+// getProducts returns the products from the data store
+func (p *Products) getProducts(w http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle GET Products")
+
+	//fetch the products from the data store
 	lp := data.GetProducts()
+
+	//serialize the list to JSON
 	err := lp.ToJSON(w)
 	if err != nil {
 		http.Error(w, "Unable to marshal json", http.StatusInternalServerError)
 	}
+}
+
+func (p *Products) addProduct(w http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle POST Product")
+
 }
